633e27e66be5097a7cf77baefe4de47c3dbc2412
KynaHui <huihoilam03@gmail.com>
2026-04-30 23:21:05 +0800

    try lecture 5 code

---
+++ lecture_code/5. memory management/5-1. sizeof.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+// sizeof(arg): give size of arg in bytes 
+// NOT number of elements
+
+// short arr[]: equivalent to short arr*, as argument
+void mystery(short arr[], int len){
+    printf("%d ", len);
+    printf("%zu\n", sizeof(arr)); // array decay to pointer
+}
+
+int main(){
+    // 1 bit = 1b, 8b = 1 byte
+    // shorts: 16b (2 bytes), pointers: 64b (8 bytes) regardless of contents
+    short nums[] = {1, 2, 3, 99, 100};
+    // sizeof(arg) returns size_t, use `%zu`: size_t unsigned
+    printf("%zu ", sizeof(nums)); // total array size 
+    mystery(nums, sizeof(nums) / sizeof(short)); // # elements 
+    return 0;
+}
